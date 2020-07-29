a32be4ffc706d539e0047c1d0ba7b5d9c18637da
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-07-29 18:10:16 +0200

    chore(ci): trick Vercel into thinking that we are not using Next.js
    All we want is to reproduce the generic CI environment and don't care about Vercel's integrations with Next.js.

---
+++ tools/remove-nextjs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+trap "exit" INT
+
+# Removes "next" from package.json dependency list
+# Reason: Trick Vercel into thinking that we are not using Next.js. All we want is to reproduce the generic CI
+# environment and don't care about Vercel's integrations with Next.js.
+sed -i.bak '/"next":/d' package.json
